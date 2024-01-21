a1e60b67474a70d17716bd5a7d18a387a1688ca2
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 14:36:45 -0500

    Add missing () to toString on line 22; now setTile() works

--- main.js
+++ main.js
@@ -7,39 +7,40 @@ const rows = 6;
 const columns = 7;
 
 window.onload = function() {
-    setGame();
-}
+  setGame();
+};
+
 function setGame() {
-    board = [];
+  board = [];
 
-    for (let r = 0; r < rows; r++) {
-        let row = [];
-        for (let c = 0; c < columns; c++) {
-            row.push(' ');
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+      row.push(' ');
 
-            let tile = document.createElement('div');
-            tile.id = r.toString + "-" + c.toString;
-            tile.classList.add('tile');
-            tile.addEventListener("click", setPiece);
-            document.getElementById('board').append(tile);
-        }
-        board.push(row);
+      let tile = document.createElement('div');
+      tile.id = r.toString() + "-" + c.toString();
+      tile.classList.add('tile');
+      tile.addEventListener("click", setPiece);
+      document.getElementById('board').append(tile);
     }
+    board.push(row);
+  }
 }
 
 function setPiece() {
-    if (gameOver) {
-        return;
-    }
-    let coords = this.id.split("-");
-    let r = parseInt(coords[0]);
-    let c = parseFloat(coords[1]);
+  if (gameOver) {
+    return;
+  }
+  let coords = this.id.split("-");
+  let r = parseInt(coords[0]);
+  let c = parseFloat(coords[1]);
 
-    board[r][c] = currentPlayer;
-    let tile = this;
-    if (currentPlayer == playerRed) {
-        tile.classList.add("red-piece");
-    } else {
-        tile.classList.add("yellow-piece");
-    }
+  board[r][c] = currentPlayer;
+  let tile = this;
+  if (currentPlayer == playerRed) {
+    tile.classList.add("red-piece");
+  } else {
+    tile.classList.add("yellow-piece");
+  }
 }
