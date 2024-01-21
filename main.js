70867fa10701fd12059b6096b774f37d9f4f0141
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 14:07:28 -0500

    Init getGame(), create a for loop in the function.

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ const board;
 const rows = 6;
 const columns = 7;
 
+window.onload = function() {
+    getGame();
+}
+function getGame() {
+    board = [];
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(``);
+
+            let tile = document.createElement('div');
+            tile.id = r.toString + "-" + c.toString;
+        }
+    }
+}
