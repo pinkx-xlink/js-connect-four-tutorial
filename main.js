ea9e6396d645f83321018964bf20678e0cfbb8e6
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 14:28:29 -0500

    Create setPiece(); doesn't add a tile like in the tutorial, time to troubleshoot some more

--- main.js
+++ main.js
@@ -20,8 +20,26 @@ function setGame() {
             let tile = document.createElement('div');
             tile.id = r.toString + "-" + c.toString;
             tile.classList.add('tile');
+            tile.addEventListener("click", setPiece);
             document.getElementById('board').append(tile);
         }
         board.push(row);
+    }
+}
+
+function setPiece() {
+    if (gameOver) {
+        return;
+    }
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseFloat(coords[1]);
+
+    board[r][c] = currentPlayer;
+    let tile = this;
+    if (currentPlayer == playerRed) {
+        tile.classList.add("red-piece");
+    } else {
+        tile.classList.add("yellow-piece");
     }
 }
