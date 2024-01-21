a5b50958770120b1ab0cf288ada18cfb3eacc7ea
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 15:00:33 -0500

    Add loop to check for a winner horizontally (lines 63-68)

--- main.js
+++ main.js
@@ -59,5 +59,16 @@ function setPiece() {
 }
 
 function checkWinner() {
-    
+    //check horizontally (sliding window) 
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != " ") {
+                if (board[r][c] == board[r][c+1] && board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
 }
