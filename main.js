d7e2448657e348f3f2f8067d7a7aacb64b7b6f6d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-01-21 15:08:59 -0500

    Edit line 66 to fix bug in horizontal checkWinner() (it was showing a winner for 2 in a row instead of 4 in a row)

--- main.js
+++ main.js
@@ -63,13 +63,16 @@ function checkWinner() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns - 3; c++) {
             if (board[r][c] != " ") {
-                if (board[r][c] == board[r][c+1] && board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                if (board[r][c] == board[r][c+1] && board[r][c+1]== board[r][c+2] && board[r][c+2] == board[r][c+3]) {
                     setWinner(r, c);
                     return;
                 }
             }
         }
     }
+
+    //check vertically (sliding window DOWN)
+
 }
 function setWinner(r, c) {
     let winner = document.getElementById("winner");
